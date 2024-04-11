95ce25e37f59ba29409491147b00523e9ea0f704
Micah123321 <69micah96@gmail.com>
2024-04-12 00:04:02 +0800

    Add .gitignore and setup_fail2ban.sh scripts

---
+++ setup_fail2ban.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# 检测系统类型
+if [ -f /etc/debian_version ]; then
+    OS="Debian"
+elif [ -f /etc/redhat-release ]; then
+    OS="CentOS"
+else
+    echo "不支持的操作系统。"
+    exit 1
+fi
+
+# Debian系列的安装和配置
+if [ "$OS" = "Debian" ]; then
+    apt-get install -y fail2ban
+    systemctl start fail2ban
+    systemctl enable fail2ban
+    sudo cp /etc/fail2ban/jail.{conf,local}
+    sudo bash -c 'echo "[DEFAULT]
+ignoreip = 127.0.0.1/8
+
+[sshd]
+enabled = true
+port = 22
+filter = sshd
+logpath = /var/log/auth.log
+bantime  = 1d
+findtime  = 5m
+maxretry = 3" > /etc/fail2ban/jail.local'
+    systemctl restart fail2ban
+    systemctl status fail2ban
+
+# CentOS系列的安装和配置
+elif [ "$OS" = "CentOS" ]; then
+    yum install fail2ban -y
+    systemctl start fail2ban
+    systemctl enable fail2ban
+    sudo cp /etc/fail2ban/jail.{conf,local}
+    sudo bash -c 'echo "[DEFAULT]
+ignoreip = 127.0.0.1/8
+
+[sshd]
+enabled = true
+port = 22
+filter = sshd
+logpath = /var/log/secure
+bantime  = 1d
+findtime  = 5m
+maxretry = 3" > /etc/fail2ban/jail.local'
+    systemctl restart fail2ban
+    systemctl status fail2ban
+fi
